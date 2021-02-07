7b1f4448048e84e5e3b939ccabee3276cea3b2ef
John Schult <johnschult@me.com>
2021-02-07 10:14:36 -0500

    Created container deploy script

--- packages/container/config/webpack.config.common.js
+++ packages/container/config/webpack.config.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   module: {
     rules: [
@@ -15,4 +17,9 @@ module.exports = {
     ],
   },
   devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 }
